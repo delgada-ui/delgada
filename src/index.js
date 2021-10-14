7011b994ed788a18660703dad485e785c5297d81
Hawk Ticehurst <hawkticehurst@gmail.com>
2021-10-13 21:37:05 -0700

    Clean up index.js and add comments

--- src/index.js
+++ src/index.js
@@ -1,22 +1,26 @@
 #!/usr/bin/env node
 'use strict';
 const { compile } = require('./compile/compile');
-const { create, copyFolderRecursiveSync, del } = require('./utils/fileio');
+const { clear, copyDir } = require('./utils/fileio');
 
 async function main() {
+  // Get command line arguments
   const entryPoint = process.argv[2];
   const buildDirectory = process.argv[3];
   const assetDirectory = process.argv[4];
 
-  del(buildDirectory);
-  create(buildDirectory);
+  // Clear the build directory if it exists
+  clear(buildDirectory);
 
   console.log('\nCompiling source code...');
 
+  // Compile and build the source code
   await compile(entryPoint, buildDirectory);
 
+  // If an asset directory path was given, copy
+  // its contents into the build directory
   if (assetDirectory) {
-    copyFolderRecursiveSync(assetDirectory, buildDirectory);
+    copyDir(assetDirectory, buildDirectory);
   }
 
   console.log('Build created 🎉\n');
