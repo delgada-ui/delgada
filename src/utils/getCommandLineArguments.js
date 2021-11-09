798dbbcf49d44f70f5fc9ff0807d20b4345c91cb
Hawk Ticehurst <hawkticehurst@gmail.com>
2021-11-08 23:13:00 -0800

    Add build command to CLI

--- src/utils/getCommandLineArguments.js
+++ src/utils/getCommandLineArguments.js
@@ -1,18 +1,24 @@
 function getCommandLineArguments() {
-  let entryPointPath = process.argv[2];
-  let buildDirectoryPath = process.argv[3];
-  let assetDirectoryPath = process.argv[4];
+  const command = process.argv[2];
 
-  // If the entry point path or build path are not
-  // provided set default paths
-  if (entryPointPath === undefined) {
-    entryPointPath = './src/index.html';
-  }
-  if (buildDirectoryPath === undefined) {
-    buildDirectoryPath = './build';
-  }
+  if (command === 'build') {
+    let entryPointPath = process.argv[3];
+    let buildDirectoryPath = process.argv[4];
+    let assetDirectoryPath = process.argv[5];
 
-  return [entryPointPath, buildDirectoryPath, assetDirectoryPath];
+    // If the entry point path or build path are not
+    // provided set default paths
+    if (entryPointPath === undefined) {
+      entryPointPath = './src/index.html';
+    }
+    if (buildDirectoryPath === undefined) {
+      buildDirectoryPath = './build';
+    }
+
+    return [entryPointPath, buildDirectoryPath, assetDirectoryPath];
+  } else {
+    throw new Error('Please specify a valid command.');
+  }
 }
 
 module.exports = {
