f92cd83dc0362522beef5dfbe6950ceefe8d85bf
Hawk Ticehurst <39639992+hawkticehurst@users.noreply.github.com>
2021-11-08 22:42:59 -0800

    Clean Up Pass (#1)
    Description of changes
    
    - Reorder the build order so the asset directory is copied before compilation
    - Abstract command-line argument logic into a utility function
    - Fix formatting errors

--- src/index.js
+++ src/index.js
@@ -1,24 +1,26 @@
-const { clear, copyDir } = require('./utils');
+const { clear, copyDir, getCommandLineArguments } = require('./utils');
 const { compile } = require('./compile');
 
 async function main() {
   // Get command line arguments
-  const entryPoint = process.argv[2];
-  const buildDirectory = process.argv[3];
-  const assetDirectory = process.argv[4];
+  const [
+    entryPoint,
+    buildDirectory,
+    assetDirectory,
+  ] = getCommandLineArguments();
 
-  // Clear the build directory if it exists
+  // Clear the build directory if it exists and
+  // create a new build directory if it does not
   clear(buildDirectory);
-
-  console.log('\nCompiling source code...');
-
-  await compile(entryPoint, buildDirectory);
 
   // If an asset directory path was given, copy
   // its contents into the build directory
   if (assetDirectory) {
     copyDir(assetDirectory, buildDirectory);
   }
+
+  // Compile and build PoppyHTML
+  await compile(entryPoint, buildDirectory);
 
   console.log('Build created 🎉\n');
 }
