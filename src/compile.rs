dd77f31181aa363b68be3a1816a9fb9859c92548
Hawk Ticehurst <hawkticehurst@gmail.com>
2021-12-23 20:24:13 -0800

    Remove compile.rs

--- src/compile.rs
+++
@@ -1,9 +0,0 @@
-use napi::{CallContext, JsString, Result};
-
-#[js_function(1)]
-pub fn compile(ctx: CallContext) -> Result<JsString> {
-  let entrypoint_path = ctx.get::<JsString>(0)?.into_utf8()?;
-  let output = format!("Entrypoint: {}", entrypoint_path.as_str()?);
-
-  ctx.env.create_string(output.as_str())
-}
