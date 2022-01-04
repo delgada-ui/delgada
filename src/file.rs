4e02e3fa6785d8ad649daf57343533cd3cc8c725
Hawk Ticehurst <hawkticehurst@gmail.com>
2022-01-04 01:46:31 -0800

    Add copy_dir_all util function

--- src/file.rs
+++ src/file.rs
@@ -1,4 +1,4 @@
-use std::path::Path;
+use std::{fs, io, path::Path};
 
 // Clear the build directory if it exists and create a
 // new build directory if it does not
@@ -8,4 +8,18 @@ pub fn clear_dir(build_dir: &String) {
     std::fs::remove_dir_all(build_dir_path).unwrap();
   }
   std::fs::create_dir_all(build_dir_path).unwrap();
+}
+
+pub fn copy_dir_all(src: impl AsRef<Path>, dst: impl AsRef<Path>) -> io::Result<()> {
+  fs::create_dir_all(&dst)?;
+  for entry in fs::read_dir(src)? {
+    let entry = entry?;
+    let ty = entry.file_type()?;
+    if ty.is_dir() {
+      copy_dir_all(entry.path(), dst.as_ref().join(entry.file_name()))?;
+    } else {
+      fs::copy(entry.path(), dst.as_ref().join(entry.file_name()))?;
+    }
+  }
+  Ok(())
 }
